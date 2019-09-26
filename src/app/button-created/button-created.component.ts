359e4f6bc8669dd371dcb0a870cfdd43b8b3ab47
dangquangvu <dangquangvu999@gmail.com>
2019-09-26 21:20:28 +0700

    form-input

--- src/app/button-created/button-created.component.ts
+++ src/app/button-created/button-created.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-button-created',
   templateUrl: './button-created.component.html',
   styleUrls: ['./button-created.component.css']
 })
-export class ButtonCreatedComponent implements OnInit {
+export class ButtonCreatedComponent implements OnInit  {
     public visiable : boolean = true;
+    public date: string = moment().format('YYYY-MM-DD');
   constructor() { }
 
   ngOnInit() {
     }
     clickbutton(event){
         this.visiable = !this.visiable;
+    }
+    dateTime(){
+        alert(this.date)
     }
 }
